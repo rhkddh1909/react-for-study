be7fe4eda7cc6b2e090d1eaa8199bf61cd7f6f78
이광오 <rhkddh1909@gmail.com>
2021-11-11 00:13:14 +0900

    모달 스타일링 끝 밑 버튼 효과 추가

--- my-app/src/UdModal.js
+++ my-app/src/UdModal.js
@@ -1,12 +1,31 @@
 import {createPortal} from "react-dom";
+import React,{useState, useEffect} from 'react';
 import './css/Modal.css';
 
 function UdModal(){
+
+    const mDown = (e) => {
+        e.target.style.background = "hsl(0, 0%, 96%)";
+    }
+    const mUp = (e) => {
+        e.target.style.background = "white";
+        alert(e.target.id);
+    }
+    const mOver = (e) =>{
+        e.target.style.background = "hsl(0, 0%, 96%)";
+    }
+    const mOut = (e) =>{
+        e.target.style.background = "white";
+    }
     return createPortal(
         <div className="udModalOverlay">
-            <div className="udModalInner">
-                <button>수정</button>
-                <button>삭제</button>
+            <div className="udModalWrapper">
+                <div className="udModalInner">
+                    <div>
+                        <div><button id="update" className="customBtn top" onMouseUp={mUp} onMouseDown={mDown} onMouseOver={mOver} onMouseOut={mOut}>수정</button></div>
+                        <div><button id="delete" className="customBtn bottom" onMouseUp={mUp} onMouseDown={mDown} onMouseOver={mOver} onMouseOut={mOut}>삭제</button></div>
+                    </div>
+                </div>
             </div>
         </div>,
         document.getElementById("modal")
